00569aa7dea7c4ddc00c505efc76da9c064f9b8a
Duke <59591006+DukeofWaterloo@users.noreply.github.com>
2024-05-29 21:57:25 -0400

    Add first version of code
    First (working!) implementation of code is in, very basic but a great start

---
+++ DayAndNight.ino
@@ -0,0 +1,60 @@
+#include <Servo.h>
+
+// Photoresistor Pin Setup
+int topPhotoPin = 2;
+int botPhotoPin = 4;
+
+int topReading;
+int botReading;
+
+// Servo Control Setup
+int currPos = 90;
+int maxPos = 135;
+int minPos = 45;
+int turnStep = 5;
+int turnTolerance = 20;
+
+Servo solarServo;
+
+void setup() {
+
+  solarServo.attach(4);  // Attach physical servo to the servo object
+
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  Serial.println("--");
+
+  // Read Analog Values from Voltage Dividers
+  int topValue = analogRead(topPhotoPin);
+  int botValue = analogRead(botPhotoPin);
+
+  if (abs(topValue - botValue) > turnTolerance) { // System in Moveable Condition (above tolerance)
+
+    if ((currPos + turnStep) < maxPos) { // Motor is Moveable Upwards
+      if (topValue > botValue) { // Stronger Light from Top
+        currPos += turnStep;
+        solarServo.write((currPos));
+      }
+    }
+    if ((currPos - turnStep) > minPos) { // Motor is Moveable Downwards
+      if (topValue < botValue) { // Stronger Light from Bottom
+        currPos -= turnStep;
+        solarServo.write((currPos));
+      }
+    }
+  }
+
+  Serial.print("Top Reading: ");
+  Serial.println(topValue);
+
+  Serial.print("Bottom Reading: ");
+  Serial.println(botValue);
+
+  Serial.println("--");
+
+  delay(250);
+
+}
